1cc6e13c601fe03a55883d7a13b1fb1db22aefdb
Blakc Mirror <blakc.mirror@gmail.com>
2026-08-22 00:07:57 +0200

    Initialise les coefficients d'atténuation dans le parseur bonus
    Le parseur bonus alloue t_light avec malloc mais ne remplit jamais
    kc, kl et kq. La fonction attenuation divise ensuite par ces valeurs
    non initialisées, ce qui assombrit ou noircit les scènes bonus au
    hasard selon le contenu de la mémoire. Même correction que la #2,
    qui ne touchait que le parseur obligatoire.

--- srcs/parser/parse_light_bonus.c
+++ srcs/parser/parse_light_bonus.c
@@ -12,10 +12,24 @@
 
 #include "miniRT_bonus.h"
 
+static void	append_light(t_scene *scene, t_light *light)
+{
+	t_light	*cursor;
+
+	if (!scene->lights)
+	{
+		scene->lights = light;
+		return ;
+	}
+	cursor = scene->lights;
+	while (cursor->next)
+		cursor = cursor->next;
+	cursor->next = light;
+}
+
 void	parse_light(char **tokens, t_scene *scene)
 {
 	t_light	*light;
-	t_light	*cursor;
 
 	if (token_count(tokens) != 4)
 		error_exit("light: expected 'L <x,y,z> <brightness> <r,g,b>'");
@@ -27,15 +41,10 @@ void	parse_light(char **tokens, t_scene *scene)
 	if (light->brightness < 0.0 || light->brightness > 1.0)
 		error_exit("light brightness must be in [0.0, 1.0]");
 	light->color = parse_color(tokens[3]);
+	light->kc = 1.0;
+	light->kl = 0.0;
+	light->kq = 0.0;
 	light->next = NULL;
-	if (!scene->lights)
-		scene->lights = light;
-	else
-	{
-		cursor = scene->lights;
-		while (cursor->next)
-			cursor = cursor->next;
-		cursor->next = light;
-	}
+	append_light(scene, light);
 	scene->has_light = 1;
 }
